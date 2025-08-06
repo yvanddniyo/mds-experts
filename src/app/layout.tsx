36f0685c1775aa69707a3f0fb2c9d7d0593eb885
yvanddniyo <nyvan80076@gmail.com>
2025-08-06 22:54:48 +0200

    Adding the metadata for the platform.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Aleo, Afacad } from "next/font/google";
+import mdsExpert from "@/assets/MDS-White.png";
 import "./globals.css";
 
 const albertSans = Aleo({
@@ -15,8 +16,32 @@ const afacode = Afacad({
 export const metadata: Metadata = {
   title: "MDS Experts Ltd",
   description: "MDS Experts Ltd is a leading Rwandan consultancy and training firm at the forefront of professional development in Africa.",
+  metadataBase: new URL('https://mdsexperts.rw'),
+  openGraph: {
+    title: "MDS Experts Ltd",
+    description: "MDS Experts Ltd is a leading Rwandan consultancy and training firm at the forefront of professional development in Africa.",
+    url: 'https://mdsexperts.rw',
+    siteName: 'MDS Experts Ltd',
+    images: [
+      {
+        url: '../assets/MDS-White.png',
+        width: 800,
+        height: 600,
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "MDS Experts Ltd",
+    description: "MDS Experts Ltd is a leading Rwandan consultancy and training firm at the forefront of professional development in Africa.",
+    images: ['../assets/MDS-White.png'],
+  },
   icons: {
     icon: "../assets/MDS-White.png",
+    shortcut: "../assets/MDS-White.png",
+    apple: "../assets/MDS-White.png",
   },
 };
 
